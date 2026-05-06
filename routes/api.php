12ccde3462b5f286745ac122b9491f05be0496b0
Towhid Hasan <towhid.hasan004@gmail.com>
2026-05-06 14:27:12 +0600

    Refactor API routes: reorganize draft routes for improved readability

--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,17 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::get('/templates', [SavedTemplateController::class, 'index']);
     Route::post('/templates', [SavedTemplateController::class, 'store']);
     Route::get('/templates/{template}', [SavedTemplateController::class, 'show']);
     Route::delete('/templates/{template}', [SavedTemplateController::class, 'destroy']);
+
     Route::get('/addresses', [SavedAddressController::class, 'index']);
     Route::post('/addresses', [SavedAddressController::class, 'store']);
     Route::put('/addresses/{address}', [SavedAddressController::class, 'update']);
     Route::delete('/addresses/{address}', [SavedAddressController::class, 'destroy']);
+    
     Route::get('/drafts', [DraftController::class, 'index']);
     Route::post('/drafts', [DraftController::class, 'store']);
     Route::get('/drafts/{draft}', [DraftController::class, 'show']);
